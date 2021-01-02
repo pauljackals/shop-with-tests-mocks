a56308e4b8d056105d64b875cbb108258b92937c
Pawel Sakaluk <paul.jackals@gmail.com>
2021-01-02 14:52:26 +0100

    testing ShopDatabase init with wrong type url

--- tests/test_shop_database.py
+++ tests/test_shop_database.py
@@ -7,6 +7,10 @@ class TestShopDatabase(unittest.TestCase):
         shop_database = ShopDatabase('http://example.com')
         self.assertIsInstance(shop_database, ShopDatabase)
 
+    def test_init_wrong_type(self):
+        with self.assertRaisesRegex(TypeError, "^Api URL must be a valid url string$"):
+            ShopDatabase(545)
+
 
 if __name__ == '__main__':
     unittest.main()

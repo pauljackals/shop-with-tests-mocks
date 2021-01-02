2733f290330c68685c182e6239ba77f6ba904caf
Pawel Sakaluk <paul.jackals@gmail.com>
2021-01-02 14:52:59 +0100

    implemented ShopDatabase init with wrong type url check

--- src/shop/shop_database.py
+++ src/shop/shop_database.py
@@ -3,5 +3,8 @@ import requests
 
 class ShopDatabase:
     def __init__(self, api_url):
-        self.api_url = api_url
-        self.request = requests.request
+        if type(api_url) != str:
+            raise TypeError("Api URL must be a valid url string")
+        else:
+            self.api_url = api_url
+            self.request = requests.request

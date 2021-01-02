e4d2dcba5e09f097ed2bf7ba6e53321caaebf39a
Pawel Sakaluk <paul.jackals@gmail.com>
2021-01-02 14:55:42 +0100

    implemented ShopDatabase init with invalid url check

--- src/shop/shop_database.py
+++ src/shop/shop_database.py
@@ -1,9 +1,10 @@
 import requests
+import re
 
 
 class ShopDatabase:
     def __init__(self, api_url):
-        if type(api_url) != str:
+        if type(api_url) != str or not re.match(r"(https?://(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?://(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})", api_url):
             raise TypeError("Api URL must be a valid url string")
         else:
             self.api_url = api_url

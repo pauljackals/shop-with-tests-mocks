f14b012763c1258ca9097aba0a41a46db9ff7ad5
Pawel Sakaluk <paul.jackals@gmail.com>
2021-01-02 17:50:00 +0100

    testing non-existent client getting

--- tests/test_shop_database.py
+++ tests/test_shop_database.py
@@ -5,14 +5,6 @@ from unittest.mock import Mock
 
 class TestShopDatabase(unittest.TestCase):
     def setUp(self):
-        def request_custom(method, url):
-            url_split = url.split('/')
-            endpoint = url_split[-2]
-            id_param = int(url_split[-1])
-            if method == 'get':
-                for entity in self.database[endpoint]:
-                    if entity['id'] == id_param:
-                        return TestResponse(entity)
         self.database = {
             'clients': [
                 {
@@ -29,6 +21,16 @@ class TestShopDatabase(unittest.TestCase):
                 }
             ]
         }
+
+        def request_custom(method, url):
+            url_split = url.split('/')
+            endpoint = url_split[-2]
+            id_param = int(url_split[-1])
+            if method == 'get':
+                for entity in self.database[endpoint]:
+                    if entity['id'] == id_param:
+                        return TestResponse(entity, 200)
+                return TestResponse({}, 404)
         self.shop_database = ShopDatabase('http://example.com')
         self.shop_database.request = Mock()
         self.shop_database.request.side_effect = request_custom
@@ -52,13 +54,18 @@ class TestShopDatabase(unittest.TestCase):
         with self.assertRaisesRegex(TypeError, "^Client ID must an integer$"):
             self.shop_database.client_get('1')
 
+    def test_client_get_missing(self):
+        with self.assertRaisesRegex(LookupError, "^Client with such ID doesn't exist$"):
+            self.shop_database.client_get(999)
+
     def tearDown(self):
         self.shop_database = None
 
 
 class TestResponse:
-    def __init__(self, value):
+    def __init__(self, value, status_code):
         self.value = value
+        self.status_code = status_code
 
     def json(self):
         return self.value

bc5ea3e3868b7ba7e610ba53b53e7ae4b9a26d26
Pawel Sakaluk <paul.jackals@gmail.com>
2021-01-02 17:01:48 +0100

    testing client getting

--- tests/test_shop_database.py
+++ tests/test_shop_database.py
@@ -1,8 +1,38 @@
 import unittest
 from src.shop.shop_database import ShopDatabase
+from unittest.mock import Mock
 
 
 class TestShopDatabase(unittest.TestCase):
+    def setUp(self):
+        def request_custom(method, url):
+            url_split = url.split('/')
+            endpoint = url_split[-2]
+            id_param = int(url_split[-1])
+            if method == 'get':
+                for entity in self.database[endpoint]:
+                    if entity['id'] == id_param:
+                        return TestResponse(entity)
+        self.database = {
+            'clients': [
+                {
+                    'id': 0,
+                    'name_first': 'John',
+                    'name_last': 'Rose',
+                    'email': 'john_rose@example.com'
+                },
+                {
+                    'id': 1,
+                    'name_first': 'Jane',
+                    'name_last': 'Blue',
+                    'email': 'jane_blue@example.com'
+                }
+            ]
+        }
+        self.shop_database = ShopDatabase('http://example.com')
+        self.shop_database.request = Mock()
+        self.shop_database.request.side_effect = request_custom
+
     def test_init(self):
         shop_database = ShopDatabase('http://example.com')
         self.assertIsInstance(shop_database, ShopDatabase)
@@ -14,6 +44,21 @@ class TestShopDatabase(unittest.TestCase):
     def test_init_invalid(self):
         with self.assertRaisesRegex(TypeError, "^Api URL must be a valid url string$"):
             ShopDatabase('http://examplecom')
+
+    def test_client_get(self):
+        id_client = 1
+        self.assertDictEqual(self.shop_database.client_get(id_client), self.database['clients'][id_client])
+
+    def tearDown(self):
+        self.shop_database = None
+
+
+class TestResponse:
+    def __init__(self, value):
+        self.value = value
+
+    def json(self):
+        return self.value
 
 
 if __name__ == '__main__':

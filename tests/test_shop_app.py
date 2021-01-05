ca36c9081ced98f9e664ffaadae58193b6fa5578
Pawel Sakaluk <paul.jackals@gmail.com>
2021-01-06 00:09:20 +0100

    testing client registration

---
+++ tests/test_shop_app.py
@@ -0,0 +1,22 @@
+import unittest
+from unittest.mock import Mock
+from src.shop.shop_app import ShopApp
+
+
+class TestShopApp(unittest.TestCase):
+    def setUp(self):
+        mock_shop_database = Mock()
+        mock_shop_database.client_post.return_value = {'id': 2}
+
+        self.shop_app = ShopApp('http://example.com')
+        self.shop_app.shop_database = mock_shop_database
+
+    def test_register_client(self):
+        self.assertEqual(self.shop_app.register_client('Henry', 'Glenn', 'henry_glenn@example.com'), 2)
+
+    def tearDown(self):
+        self.shop_app = None
+
+
+if __name__ == '__main__':
+    unittest.main()

fd33a635107c0bbf5f27a745a9744bb6146ee5d5
Pawel Sakaluk <paul.jackals@gmail.com>
2021-01-06 00:12:43 +0100

    testing client download

--- tests/test_shop_app.py
+++ tests/test_shop_app.py
@@ -5,8 +5,61 @@ from src.shop.shop_app import ShopApp
 
 class TestShopApp(unittest.TestCase):
     def setUp(self):
+        self.database_simplified = {
+            'clients': [
+                {
+                    'id': 0,
+                    'name_first': 'John',
+                    'name_last': 'Rose',
+                    'email': 'john_rose@example.com'
+                },
+                {
+                    'id': 1,
+                    'name_first': 'Jane',
+                    'name_last': 'Blue',
+                    'email': 'jane_blue@example.com'
+                }
+            ],
+            'items': [
+                {
+                    'id': 0,
+                    'name': 'PlayStation 4 Slim',
+                    'value': 1288.00
+                },
+                {
+                    'id': 1,
+                    'name': 'Xbox One S',
+                    'value': 1049.99
+                },
+                {
+                    'id': 2,
+                    'name': 'Nintendo Switch',
+                    'value': 1479.00
+                }
+            ],
+            'orders': [
+                {
+                    'id': 0,
+                    'id_client': 0,
+                    'ids_items': [2]
+                },
+                {
+                    'id': 1,
+                    'id_client': 1,
+                    'ids_items': [1, 0]
+                }
+            ]
+        }
+
+        def get_side_effect(endpoint, id_entity):
+            if id_entity is None:
+                return self.database_simplified[endpoint]
+            else:
+                return self.database_simplified[endpoint][id_entity]
+
         mock_shop_database = Mock()
         mock_shop_database.client_post.return_value = {'id': 2}
+        mock_shop_database.client_get.side_effect = lambda id_client=None: get_side_effect('clients', id_client)
 
         self.shop_app = ShopApp('http://example.com')
         self.shop_app.shop_database = mock_shop_database
@@ -19,8 +72,13 @@ class TestShopApp(unittest.TestCase):
         self.shop_app.register_client(*params)
         self.shop_app.shop_database.client_post.assert_called_with(*params)
 
+    def test_download_client(self):
+        client = self.database_simplified['clients'][1]
+        self.assertDictEqual(self.shop_app.download_client(client['id']), client)
+
     def tearDown(self):
         self.shop_app = None
+        self.database_simplified = None
 
 
 if __name__ == '__main__':

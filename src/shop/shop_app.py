07c33fb2c260601bcf809d34710a9870ec2fa4bb
Pawel Sakaluk <paul.jackals@gmail.com>
2021-01-06 00:13:02 +0100

    implemented client download

--- src/shop/shop_app.py
+++ src/shop/shop_app.py
@@ -8,3 +8,7 @@ class ShopApp:
     def register_client(self, name_first, name_last, email):
         client = self.shop_database.client_post(name_first, name_last, email)
         return client['id']
+
+    def download_client(self, id_client):
+        client = self.shop_database.client_get(id_client)
+        return client

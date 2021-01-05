4ad56c1df1569cd82027fc54cedba32e9cea26d9
Pawel Sakaluk <paul.jackals@gmail.com>
2021-01-06 00:09:50 +0100

    added client registration

---
+++ src/shop/shop_app.py
@@ -0,0 +1,10 @@
+from .shop_database import ShopDatabase
+
+
+class ShopApp:
+    def __init__(self, api_url):
+        self.shop_database = ShopDatabase(api_url)
+
+    def register_client(self, name_first, name_last, email):
+        client = self.shop_database.client_post(name_first, name_last, email)
+        return client['id']
